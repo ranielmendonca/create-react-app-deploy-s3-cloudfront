a79ccc09e73ea556e61d7aa2d7e292189792417e
Raniel Mendonça <raniel.mendonca@zup.com.br>
2022-01-14 11:45:51 -0300

    feat: convert v5 to v6 reacr router dom

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
 import {
   BrowserRouter as Router,
-  Switch,
+  Routes,
   Route,
   Link
 } from "react-router-dom";
+
 
 function Home() {
   return <h2>Home</h2>;
@@ -28,43 +29,35 @@ function App() {
   return (
     <div className="App">
       <Router>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-            <li>
-              <Link to="/users">Users</Link>
-            </li>
-            <li>
-              <Link to="/dashboard">Dashboard</Link>
-            </li>
-          </ul>
-        </nav>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/about">About</Link>
+              </li>
+              <li>
+                <Link to="/users">Users</Link>
+              </li>
+              <li>
+                <Link to="/dashboard">Dashboard</Link>
+              </li>
+            </ul>
+          </nav>
 
-        {/* A <Switch> looks through its children <Route>s and
+          {/* A <Routes> looks through its children <Route>s and
             renders the first one that matches the current URL. */}
-        <Switch>
-          <Route path="/about">
-            <About />
-          </Route>
-          <Route path="/users">
-            <Users />
-          </Route>
-          <Route path="/dashboard">
-            <Dashboard />
-          </Route>
-          <Route path="/">
-            <Home />
-          </Route>
-          
-        </Switch>
-      </div>
-    </Router>
+          <Routes>
+            <Route path='/about' element={<About />} />
+            <Route path='/users' element={<Users />} />
+            <Route path='/dashboard' element={<Dashboard />} />
+            <Route path='/home' element={<Home />} />
+            <Route path='/' element={<Home />} />
+          </Routes>
+        </div>
+      </Router>
     </div>
   );
 }
